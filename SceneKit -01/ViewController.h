b8652d478d74201ae9822e068a1561918dad4fb1
Chenshiwen <cswzmg@live.com>
2017-06-13 17:35:18 +0800

    简单加载3D模型

--- SceneKit -01/ViewController.h
+++ SceneKit -01/ViewController.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-
+#import <SceneKit/SceneKit.h>
 @interface ViewController : UIViewController
 
 
--- SceneKit -01/ViewController.m
+++ SceneKit -01/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
+
 @interface ViewController ()
+
+@property (nonatomic,strong) SCNView *scenView;
 
 @end
 
@@ -16,10 +19,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.view addSubview:self.scenView];
+    
 }
 
-
+-(SCNView*)scenView{
+    if (!_scenView) {
+        _scenView = [[SCNView alloc] initWithFrame:CGRectMake(0, 0, 300, 300)];
+        _scenView.center = self.view.center;
+        NSURL *url = [[NSBundle mainBundle] URLForResource:@"yizi" withExtension:@"dae"];
+        _scenView.scene = [SCNScene sceneWithURL:url options:nil error:nil];
+        _scenView.backgroundColor = [UIColor blackColor];
+        //开启可交互
+        _scenView.allowsCameraControl = YES;
+        
+    }
+    return _scenView;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
